7396bd5d557ff63e61543585ed5ba350fd72df46
JasmineNasywa <jasminenasywa6@gmail.com>
2024-09-25 21:27:35 +0700

    PemilihanTernaryOp

--- PemilihanBilangan12.java
+++ PemilihanBilangan12.java
@@ -7,14 +7,6 @@ public class PemilihanBilangan12 {
         System.out.println("Masukkan sebuah angka : ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-
-        } 
-        else
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String pemilihan = (angka % 2 == 0) ? "Angka " + angka + " termasuk bilangan genap" : "Angka " + angka + " termasuk bilangan ganjil";
     }
 }
